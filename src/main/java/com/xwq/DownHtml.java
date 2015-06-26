1d5ba9944752843f51032455005f84f10265f664
xwq <xwqfudimo@163.com>
2015-06-26 10:41:15 +0800

    first commit

---
+++ src/main/java/com/xwq/DownHtml.java
@@ -0,0 +1,13 @@
+package com.xwq;
+
+import java.net.URL;
+
+public class DownHtml {
+	public static void main(String[] args) throws Exception {
+		String url_str = "";
+		
+		URL url = new URL(url_str);
+		
+		
+	}
+}
